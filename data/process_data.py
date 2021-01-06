a473cc8e19c242a7fecc141e8695bf86c766d469
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-06 21:00:42 +0100

    feat: Initial commit + Update process_data
    Import libraries and load datasets
    Merge datasets
    Split categories into separate category columns
    Convert category values to just numbers 0 or 1.

---
+++ data/process_data.py
@@ -0,0 +1,56 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(messages_filepath, categories_filepath):
+    pass
+
+
+def clean_data(df):
+    messages = pd.read_csv('data/disaster_messages.csv')
+    categories = pd.read_csv('data/disaster_categories.csv')
+    df = messages.merge(categories, how='inner')
+    categories = df['categories'].str.split(pat=';', expand=True)
+
+    row = categories.head(1)
+    
+    extract_col_names = lambda x: x.str.split('-')[0][0]
+    category_colnames = categories.apply(extract_col_names)
+    
+    
+    pass
+
+
+def save_data(df, database_filename):
+    pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
