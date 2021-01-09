f93fb60fca23c8a4f3f76f44474fe4d160f6beac
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-09 21:38:52 +0100

    feat: finalise load_data() function: Define feature and target variables X and Y

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,6 +1,5 @@
 import sys
 from typing import Union, Iterator
-
 import pandas as pd
 from pandas import DataFrame
 from sqlalchemy import create_engine
@@ -8,9 +7,13 @@ from sqlalchemy import create_engine
 
 def load_data(database_filepath):
     engine = create_engine('sqlite:/// {}'.format(database_filepath))
-    df = pd.read_sql('data', engine)
+    df = pd.read_sql('data', engine)  # Load data from database to pandas DataFrame
+    categories = df.select_dtypes(include=['int64'])  # Select only int64 datatypes
+    categories = categories.drop('id', axis=1)  # Drop id column as irrelevant
+    X = df['message']
+    Y = categories
 
-    return df
+    return X, Y
 
 
 def tokenize(text):
@@ -35,13 +38,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -51,9 +54,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
