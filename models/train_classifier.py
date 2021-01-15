8b182c04011dab5ac1b576b4f42b1779463258df
Yesbol <yesbol_gabdullin@hotmail.com>
2021-01-15 08:04:30 +0100

    feat: Add GridSearch and save the GridSearch model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -14,8 +14,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from time import time
 import gzip, pickle, pickletools
 nltk.download(['stopwords', 'wordnet', 'punkt'])
 
@@ -75,16 +77,41 @@ def build_model():
     return pipeline
 
 
+def build_model_GridSearch():
+    classifier = MultiOutputClassifier(RandomForestClassifier())
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', classifier)
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'tfidf__norm': ('l1', 'l2'),
+        'clf__estimator__min_samples_split': [2, 4, 6],
+        'clf__estimator__max_depth': [None, 80, 90, 100],
+        'clf__estimator__n_estimators': [50, 100, 200, 300, 1000]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1)
+
+    return cv
+
+
 def evaluate_model(model, X_test, Y_test, category_names):
     y_pred = model.predict(X_test)  # Predict model
 
     for i in range(len(category_names)):
-        print("Category: {}\n {}\n {}\n______".format(
+        print("Category: {}\n Accuracy: {:.3f}\t Precision: {:.3f}\t Recall: {:.3f} \n\n".format(
             category_names[i],
-            classification_report(Y_test[:, i], y_pred[:, i]),
-            confusion_matrix(Y_test[:, i], y_pred[:, i])
-
-        ))
+            accuracy_score(Y_test[:, i], y_pred[:, i]),
+            precision_score(Y_test[:, i], y_pred[:, i]),
+            recall_score(Y_test[:, i], y_pred[:, i])
+        )
+        )
 
 
 def save_model(model, model_filepath):
@@ -96,8 +123,8 @@ def save_model(model, model_filepath):
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
+    if len(sys.argv) == 4:
+        database_filepath, model_filepath, cv_model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -116,11 +143,24 @@ def main():
 
         print('Trained model saved!')
 
+        # Grid Search
+        print('Building model with GridSearch...')
+        model_cv = build_model_GridSearch()
+
+        print('Training model GridSearch...')
+        model_cv.fit(X_train, Y_train)
+
+        print('Evaluating model...')
+        evaluate_model(model_cv, X_test, Y_test, category_names)
+
+        print('Saving GridSearch model...\n    MODEL: {}'.format(cv_model_filepath))
+        save_model(model_cv, cv_model_filepath)
+
     else:
         print('Please provide the filepath of the disaster messages database '
               'as the first argument and the filepath of the pickle file to ' 
               'save the model to as the second argument. \n\nExample: python ' 
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl gridsearch.pkl')
 
 
 if __name__ == '__main__':
